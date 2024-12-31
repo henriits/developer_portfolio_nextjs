3b164fbd53c907ac1b75895a72fd3847c053e709
henriits <henritsarents@hotmail.com>
2024-12-31 16:51:42 +0200

    fix: id needs to be awaited

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -6,7 +6,7 @@ export async function GET(
     { params }: { params: { id: string } }
 ) {
     try {
-        const { id } = params;
+        const { id } = await params;
         const project = await prisma.project.findUnique({
             where: { id: parseInt(id) },
         });
