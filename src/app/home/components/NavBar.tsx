045f87d7f8b3be32c0401a92f1cd480c46679a4c
henriits <henritsarents@hotmail.com>
2025-01-09 18:43:29 +0200

    feat: enhance Navbar with responsive design

--- src/app/home/components/NavBar.tsx
+++ src/app/home/components/NavBar.tsx
@@ -1,55 +1,55 @@
-// src/components/Navbar.tsx
+"use client";
 
+import { useState } from "react";
 import Link from "next/link";
-import { motion } from "framer-motion"; // Import Framer Motion
+import { motion } from "framer-motion";
+import { HamburgerMenuIcon, Cross1Icon } from "@radix-ui/react-icons";
+
+import { ReactNode } from "react";
+
+const NavLink = ({ href, children }: { href: string; children: ReactNode }) => (
+    <motion.div whileHover={{ scale: 1.3 }}>
+        <Link
+            href={href}
+            className="border-2 hover:text-[#13DF14] text-white py-2 px-4 rounded-lg transition duration-300 relative overflow-hidden"
+        >
+            {children}
+        </Link>
+    </motion.div>
+);
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <nav className=" bg-opacity-10 backdrop-blur-md text-white p-4 shadow-md fixed top-0 left-0 right-0 z-50">
-            <div className="container mx-auto flex justify-center items-center ">
-                <ul className="flex space-x-4 ">
-                    <li>
-                        <motion.div whileHover={{ scale: 1.3 }}>
-                            <Link
-                                href="/"
-                                className="border-2 hover:text-[#13DF14] text-white py-2 px-4 rounded-lg  transition duration-300 relative overflow-hidden"
-                            >
-                                Home
-                            </Link>
-                        </motion.div>
-                    </li>
-                    <li>
-                        <motion.div whileHover={{ scale: 1.3 }}>
-                            <Link
-                                href="/projects"
-                                className="border-2 hover:text-[#13DF14] text-white py-2 px-4 rounded-lg  transition duration-300 relative overflow-hidden"
-                            >
-                                Projects
-                            </Link>
-                        </motion.div>
-                    </li>
-                    <li>
-                        <motion.div whileHover={{ scale: 1.3 }}>
-                            <Link
-                                href="/#about"
-                                className="border-2 hover:text-[#13DF14] text-white py-2 px-4 rounded-lg  transition duration-300 relative overflow-hidden"
-                            >
-                                About
-                            </Link>
-                        </motion.div>
-                    </li>
-                    <li>
-                        <motion.div whileHover={{ scale: 1.3 }}>
-                            <Link
-                                href="/#contact"
-                                className="border-2 hover:text-[#13DF14] text-white py-2 px-4 rounded-lg  transition duration-300 relative overflow-hidden"
-                            >
-                                Contact
-                            </Link>
-                        </motion.div>
-                    </li>
-                </ul>
+        <nav className="bg-opacity-10 backdrop-blur-md text-white p-4 shadow-md fixed top-0 left-0 right-0 z-50">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link href="/" className="text-2xl font-bold">
+                    <span className="text-[#13DF14]">H</span>Tsarents
+                </Link>
+                <div className="hidden md:flex space-x-4">
+                    <NavLink href="/">Home</NavLink>
+                    <NavLink href="/projects">Projects</NavLink>
+                    <NavLink href="/#about">About</NavLink>
+                    <NavLink href="/#contact">Contact</NavLink>
+                </div>
+                <button
+                    className="md:hidden text-white"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    {isOpen ? <Cross1Icon /> : <HamburgerMenuIcon />}
+                </button>
             </div>
+            {isOpen && (
+                <div className="md:hidden">
+                    <div className="flex flex-col items-center space-y-4 m-6 gap-2 py-12">
+                        <NavLink href="/">Home</NavLink>
+                        <NavLink href="/projects">Projects</NavLink>
+                        <NavLink href="/#about">About</NavLink>
+                        <NavLink href="/#contact">Contact</NavLink>
+                    </div>
+                </div>
+            )}
         </nav>
     );
 };
