275e206bc1de733db158178e43f98fd3bdf81dc0
henriits <henritsarents@hotmail.com>
2025-01-23 20:01:23 +0200

    feat: update Slide component to improve visibility

--- src/components/animations/Slide.tsx
+++ src/components/animations/Slide.tsx
@@ -9,36 +9,43 @@ type Props = {
 
 export default function Slide({ children, delay, className }: Props) {
     const ref = useRef(null);
-    const isInView = useInView(ref); // Removed `{ once: true }` to allow continuous triggering
+    const isInView = useInView(ref);
     const controls = useAnimation();
 
     useEffect(() => {
         if (isInView) {
             controls.start("visible");
         } else {
-            controls.start("hidden"); // Return to hidden state when out of view
+            controls.start("hidden");
         }
     }, [isInView, controls]);
 
     return (
-        <motion.div
-            ref={ref}
-            variants={{
-                hidden: { opacity: 0, translateX: 90 },
-                visible: { opacity: 1, translateX: 0 },
-            }}
-            transition={{
-                type: "spring",
-                duration: 0.2,
-                damping: 4,
-                delay: delay,
-                stiffness: 100,
-            }}
-            initial="hidden"
-            animate={controls}
-            className={className}
-        >
-            {children}
-        </motion.div>
+        <div className={className}>
+            {" "}
+            {/* Added wrapper div */}
+            <motion.div
+                ref={ref}
+                variants={{
+                    hidden: { opacity: 0, translateY: 50 },
+                    visible: { opacity: 1, translateY: 0 },
+                }}
+                transition={{
+                    type: "spring",
+                    duration: 0.2,
+                    damping: 4,
+                    delay: delay,
+                    stiffness: 100,
+                }}
+                style={{
+                    visibility: isInView ? "visible" : "hidden",
+                }}
+                initial="hidden"
+                animate={controls}
+                className={!isInView ? "hidden-content" : ""}
+            >
+                {children}
+            </motion.div>
+        </div>
     );
 }
