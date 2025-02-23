3d90900062132668db3dc41241afcbef475061c2
henriits <henritsarents@gmail.com>
2025-02-23 12:49:59 +0200

    refactor: move SkillsData to a separate file for better organization and maintainability

---
+++ src/utils/skillData.ts
@@ -0,0 +1,37 @@
+import CSS from "@/../public/img/icons/css.svg";
+import Express from "@/../public/img/icons/express.svg";
+import FramerMotion from "@/../public/img/icons/framermotion.svg";
+import Git from "@/../public/img/icons/git.svg";
+import Github from "@/../public/img/icons/github.svg";
+import HTML from "@/../public/img/icons/html.svg";
+import JS from "@/../public/img/icons/javascript.svg";
+import NextJs from "@/../public/img/icons/nextjs.svg";
+import Node from "@/../public/img/icons/node.svg";
+import NPM from "@/../public/img/icons/npm.svg";
+import Postgres from "@/../public/img/icons/postgres.svg";
+import Prisma from "@/../public/img/icons/prisma.svg";
+import ReactIcon from "@/../public/img/icons/react.svg";
+import Tailwindcss from "@/../public/img/icons/tailwindcss.svg";
+import TypeScript from "@/../public/img/icons/typescript.svg";
+import VScode from "@/../public/img/icons/vscode.svg";
+import Vue from "@/../public/img/icons/vue.svg";
+
+export const SkillsData: Array<{ icon: any; label: string }> = [
+    { icon: JS, label: "JavaScript" },
+    { icon: ReactIcon, label: "React" },
+    { icon: HTML, label: "HTML5" },
+    { icon: Tailwindcss, label: "Tailwind CSS" },
+    { icon: TypeScript, label: "TypeScript" },
+    { icon: NextJs, label: "Next.js" },
+    { icon: Node, label: "Node.js" },
+    { icon: NPM, label: "npm" },
+    { icon: FramerMotion, label: "Framer Motion" },
+    { icon: Git, label: "Git" },
+    { icon: CSS, label: "CSS3" },
+    { icon: VScode, label: "VS Code" },
+    { icon: Github, label: "GitHub" },
+    { icon: Vue, label: "Vue.js" },
+    { icon: Prisma, label: "Prisma" },
+    { icon: Postgres, label: "Postgres" },
+    { icon: Express, label: "Express" },
+];
